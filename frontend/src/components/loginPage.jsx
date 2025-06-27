19a6daa4a8d85065c166a263a0415b0cec646309
CrisleyWolgien <Crisleyws@gmail.com>
2025-06-27 17:43:14 -0300

    front iniciado

---
+++ frontend/src/components/loginPage.jsx
@@ -0,0 +1,16 @@
+function LoginPage() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Login</h1>
+          <div className="">
+            <form action=""></form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;

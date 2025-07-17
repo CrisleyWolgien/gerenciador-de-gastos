d4db7db163aab3daef40b552dff44a1616faa893
CrisleyWolgien <Crisleyws@gmail.com>
2025-07-17 20:51:32 -0300

    .

--- backend/schemas/UsersSchema.py
+++ backend/schemas/UsersSchema.py
@@ -4,13 +4,11 @@ from typing import Optional
 class createUser(BaseModel):
     name : str
     email : EmailStr
-    valueLimit: float
     password: str
 
 class updateUser(BaseModel):
     name : Optional[str]
     email : Optional[EmailStr]
-    valueLimit: Optional[float]
     password: Optional[str]
 
 class loginUser(BaseModel):
